d5f889c2249bf206447543e1ac2333c82c9bf606
Milana Troyanova <troyanovamilana21@gmail.com>
2026-09-10 18:12:43 +0300

    Сделана п1.2.2

---
+++ 122-led-button/main.c
@@ -0,0 +1,33 @@
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/regs/addressmap.h"
+#include "hardware/regs/sio.h"
+
+const uint BUTTON_PIN = 15;
+const uint LED_PIN = 25;
+
+int main()
+{
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_OUT);
+    gpio_pull_up(BUTTON_PIN);
+    
+    bool led = false;
+    bool previous = false;
+
+    while (1)
+       {
+           bool current = gpio_get(BUTTON_PIN);
+
+           if (previous == true && current == false)
+           {
+               led = !led;
+               gpio_put(LED_PIN, led);
+           }
+
+           previous = current;
+       }
+    }
